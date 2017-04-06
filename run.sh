13fc9e5d471562826a0f9002fa6233808228c70e
Will <yh@fir.im>
2017-04-06 11:15:05 +0800

    change

--- run.sh
+++ run.sh
@@ -12,12 +12,21 @@
 #
 # ************************************************************
 
-flow_cmd "echo $FLOW_PROJECT_NAME" --echo
+echo '===============DEFAULT ENVS======================='
+
+echo "\$FLOW_PROJECT_NAME = $FLOW_PROJECT_NAME"
+echo "\$FLOW_USER_ID = $FLOW_USER_ID"
+echo "\$FLOW_PROJECT_ID = $FLOW_PROJECT_ID"
+echo "\$FLOW_PROJECT_GIT_URL = $FLOW_PROJECT_GIT_URL"
+echo "\$FLOW_PROJECT_LANGUAGE = $FLOW_PROJECT_LANGUAGE"
+echo "\$FLOW_GIT_HOST = $FLOW_GIT_HOST"
+echo "\$FLOW_JOB_ID = $FLOW_JOB_ID"
+echo "\$FLOW_EVENT_ID = $FLOW_EVENT_ID"
+echo "\$FLOW_EVENT_NUMBER = $FLOW_EVENT_NUMBER"
+echo "\$FLOW_PHP_OLD_VERSION = $FLOW_PHP_OLD_VERSION"
+
 flow_cmd "mvn --version" --echo
 flow_cmd "java -version" --echo
-flow_cmd "echo $FLOW_VERSION" --echo
-flow_cmd "echo $JAVA_HOME" --echo
-flow_cmd "echo $MAVEN_HOME" --echo
 
 
 FLOW_JAVA_VERSION="$(java -version 2>&1)"
